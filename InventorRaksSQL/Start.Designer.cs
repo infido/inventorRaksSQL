0de638f2ee70f687a7495325711d4c6d50363ab9
Jacek Dziwoki <j.dziwoki@infido.pl>
2015-12-25 14:10:26 +0100

    Nowy projekt z połaczeniem

---
+++ InventorRaksSQL/Start.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace InventorRaksSQL
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Start));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.konfiguracjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 395);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(687, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.konfiguracjaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(687, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // konfiguracjaToolStripMenuItem
+            // 
+            this.konfiguracjaToolStripMenuItem.Name = "konfiguracjaToolStripMenuItem";
+            this.konfiguracjaToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.konfiguracjaToolStripMenuItem.Text = "&Konfiguracja";
+            this.konfiguracjaToolStripMenuItem.Click += new System.EventHandler(this.konfiguracjaToolStripMenuItem_Click);
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 417);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Start";
+            this.Text = "InventorRaksSQL";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem konfiguracjaToolStripMenuItem;
+    }
+}
+
---
+++ InventorRaksSQL/Start.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace InventorRaksSQL
+{
+    public partial class Start : Form
+    {
+        public ConnectionDB polaczenie;
+        private FbDataAdapter fda;
+        private DataSet fds;
+        private DataView fDataView;
+        private String strQuery;
+        private String tabela;
+        private Dictionary<string, string> cascheStatus;
+        private string kolumna;
+
+        private Int32 memberRow = -1;
+        private Int32 memberColumn = -1;
+        
+        public Start()
+        {
+            InitializeComponent();
+            polaczenie = new ConnectionDB();
+            Text = "InventorRaksSQL " + Application.ProductVersion;
+        }
+
+        private void konfiguracjaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            polaczenie.Show();
+        }
+    }
+}
